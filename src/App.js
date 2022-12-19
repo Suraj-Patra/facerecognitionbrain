9418d743c1f2f3d44dafa9b026f343a648b0ce2e
Suraj Patra <patrasuraj2000@gmail.com>
2022-12-19 11:53:17 +0530

    Adding the css and styles to display the box around the face according to the detection. This app is not able to detect multiple faces now and not super optimized yet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import ParticlesBg from 'particles-bg';
-// import Clarifai from 'clarifai';
 
 import Navigation from './components/Navigation/Navigation.jsx';
 import Logo from './components/Logo/Logo.jsx';
@@ -9,31 +8,39 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.jsx';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.jsx';
 import './App.css';
 
-// const app = new Clarifai.App({
-//  apiKey: '9eedcc0643464daf8490030de1571f63'
-// });
-
-
 function App() {
   const [input, setInput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
   
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+  const displayFaceBox = (box) => {
+    setBox(box);
+  }
   const onInputChange = (e) => {
     setInput(e.target.value);
   }
   const onButtonSubmit = () => {
     setImageUrl(input);
-    // app.models.predict(Clarifai.FACE_DETECT_MODEL, 'https://sample.clarifai.com/face-det.jpg').then(
-    //   function(response){
-    //     console.log(response);
-    //   },
-    //   function(err){
-    //     console.log(err);
-    //   }
-    // );
 
-
+    // The old technique which is shown in the video wouldn't work. You have to use the new technique. Here you do not have to install clarifai or import it. 
     // Resource for the docs and solution : https://docs.clarifai.com/api-guide/predict/images/
+    // Sample image links : 
+    // https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528
+    // https://www.faceapp.com/static/img/content/compare/beard-example-before@3x.jpg
 
     const USER_ID = 'trasuraj';
     // Your PAT (Personal Access Token) can be found in the portal under Authentification
@@ -75,7 +82,7 @@ function App() {
 
     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
         .then(response => response.json())
-        .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+        .then(result => displayFaceBox( calculateFaceLocation(result) ))
         .catch(error => console.log('error', error));
     
   }
@@ -87,9 +94,14 @@ function App() {
       <Logo />
       <Ranks />
       <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
-      <FaceRecognition imageUrl={imageUrl} />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// Problems in this app are : not optimized, can't detect multiple faces
--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,11 +1,15 @@
+import './FaceRecognition.css';
 
 const FaceRecognition = (props) => {
-	const { imageUrl } = props;
+	const { imageUrl, box } = props;
+	const {leftCol, topRow, rightCol, bottomRow} = box;
+
 	return(
 		<>
 			<div className='center ma'>
 				<div className='absolute mt2'>
-					<img src={imageUrl} alt='face-detection' width='500px' height='auto' />
+					<img id='inputImage' src={imageUrl} alt='face-detection' width='500px' height='auto' />
+					<div className='bounding-box' style={{top: topRow, right: rightCol, bottom: bottomRow, left: leftCol}}></div>
 				</div>
 			</div>	
 		</>
